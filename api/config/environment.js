44e8379edb5b6cddf2613eccdd188758bfd88bd7
manhtiendev <manhtiendev1307@gmail.com>
2024-07-02 10:21:37 +0700

    Create Signin API route

--- api/config/environment.js
+++ api/config/environment.js
@@ -3,5 +3,7 @@ import 'dotenv/config';
 export const env = {
   MONGO_URI: process.env.MONGO_URI,
   APP_PORT: process.env.APP_PORT,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
   BUILD_MODE: process.env.BUILD_MODE,
 };
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,31 @@
+import { StatusCodes } from 'http-status-codes';
 import User from '~/models/user.model';
+import ApiError from '~/utils/ApiError';
 import catchAsync from '~/utils/catchAsync';
+import jwt from 'jsonwebtoken';
+import { env } from '~/config/environment';
+import bcryptjs from 'bcryptjs';
 
 export const signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
   res.status(201).json({
     message: 'success',
     data: newUser,
+  });
+});
+
+export const signin = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  const validUser = await User.findOne({ email });
+  if (!validUser) return next(new ApiError(StatusCodes.NOT_FOUND, 'User not found'));
+  const validPassword = bcryptjs.compareSync(password, validUser.password);
+  if (!validPassword) return next(new ApiError(StatusCodes.UNAUTHORIZED, 'Wrong credentials'));
+  const token = jwt.sign({ id: validUser._id }, env.JWT_SECRET, {
+    expiresIn: env.JWT_EXPIRES_IN,
+  });
+  const { password: pass, ...rest } = validUser._doc;
+  res.cookie('access_token', token, { httpOnly: true }).status(StatusCodes.OK).json({
+    status: 'success',
+    user: rest,
   });
 });
