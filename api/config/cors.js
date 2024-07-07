de339ef369c47355c0b205807f8ad621e28038a7
manhtiendev <manhtiendev1307@gmail.com>
2024-07-07 22:12:51 +0700

    fix

--- api/config/cors.js
+++ api/config/cors.js
@@ -2,7 +2,10 @@ import { env } from '~/config/environment';
 import { StatusCodes } from 'http-status-codes';
 import ApiError from '~/utils/ApiError';
 
-const WHITELIST_DOMAINS = ['http://localhost:5173'];
+const WHITELIST_DOMAINS = [
+  'http://localhost:5173',
+  'https://estate-marketplace-guzr.onrender.com/',
+];
 
 export const corsOptions = {
   origin: function (origin, callback) {
